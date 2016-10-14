5ada4e9bc06fc3ac1450466942e7a0158763a126
takeshitou <takeshitou@gmail.com>
2016-10-14 06:02:20 +0000

    customize relation modify

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,9 +4,9 @@ class Member < ActiveRecord::Base
     has_many :booking_comments
     has_many :member_pictures
     #受信メッセージリレーション
-    has_many :in_msgs , :class => "Message" , :foreign_key => :receiver
+    #has_many :in_msgs , :class => "Message" , :foreign_key => :receiver
     #送信メッセージリレーション
-    has_many :out_msgs , :class => "Message" , :foreign_key => :sender
+    #has_many :out_msgs , :class => "Message" , :foreign_key => :sender
     has_one :traveler
     has_one :guide
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ActiveRecord::Base
     
     #送信者リレーション
-    belongs_to :out_msg , :class => "Member" , :primary_key => :sender
+    #belongs_to :out_msg , :class => "Member" , :primary_key => :sender
     #受信者リレーション
-    belongs_to :in_msg , :class => "Member" , :primary_key => :receiver
+    #belongs_to :in_msg , :class => "Member" , :primary_key => :receiver
     
 end
