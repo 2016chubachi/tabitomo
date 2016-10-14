e3ff6843c73c3f30011ad69322b64aebd315fdb7
takeshitou <takeshitou@gmail.com>
2016-10-14 08:53:05 +0000

    relation modify

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,9 +4,9 @@ class Member < ActiveRecord::Base
     has_many :booking_comments
     has_many :member_pictures
     #送信メッセージリレーション
-    has_many :send_msgs , :class => "Message" , :foreign_key => "sender"
+    has_many :send_msgs , :class => 'Message' , :foreign_key => 'sender'
     #受信メッセージリレーション
-    has_many :receive_msgs , :class => "Message" , :foreign_key => "receiver"
+    has_many :receive_msgs , :class => 'Message' , :foreign_key => 'receiver'
     has_one :traveler
     has_one :guide
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ActiveRecord::Base
     
     #送信者リレーション
-    belongs_to :send_member , :class => "Member" , :foreign_key => "sender"
+    belongs_to :send_member , :class => "Member" , :foreign_key => "sender", :foreign_type => "integer"
     #受信者リレーション
-    belongs_to :receive_member , :class => "Member" , :foreign_key => "receiver"
+    belongs_to :receive_member , :class => "Member" , :foreign_key => "receiver", :foreign_type => "integer"
     
 end
