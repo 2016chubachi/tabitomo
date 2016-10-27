8cc68bdaa5f20b4b4d5035227f50496e2b8cd0f4
aprisun <aprisun@gmail.com>
2016-10-27 21:16:58 +0900

    modified for mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # mail setting.
+    host= 'tabitomo.herokuapp.com'
+    config.action_mailer.default_url_options = { host: 'tabitomo.herokuapp.com' }
+
+  # mail setting
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => "tabitomo2100@gmail.com",
+      :password => "aA@2253880",
+      :authentication => :plain,
+      :enable_starttls_auto => true
 end
