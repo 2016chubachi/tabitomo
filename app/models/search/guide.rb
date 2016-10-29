a683b6b6f56ccc4462108df6e135f393f4859e82
takeshitou <takeshitou@gmail.com>
2016-10-29 15:00:44 +0900

    comment modify

--- app/models/search/guide.rb
+++ app/models/search/guide.rb
@@ -10,17 +10,24 @@ class Search::Guide < Search::Base
 
   def selectBySearch
     guides = ::Guide.all
+    # ライセンスのチェック状況によって、条件を付く
     if license_has == "1" && license_not == "0"
+      # ライセンス有りだけがチェックされた場合
       guides = guides.where(license_flg: 1)
     elsif license_has == "0" && license_not == "1"
+      # ライセンス無だけがチェックされた場合
       guides = guides.where(license_flg: 0)
     end
     if city.present?
+      # GuideCityをjoinして検索する
       guides = guides.includes(:guide_cities).where(guide_cities: {city_master_id: city}) 
     end
+    # 性別のチェック状況によって、条件を付く
     if gender_man == "1" && gender_woman == "0"
+      # 男性だけがチェックされた場合
       guides = guides.includes(:member).where(members: {gender: 1}) 
     elsif gender_man == "0" && gender_woman == "1"
+      # 女性だけがチェックされた場合
       guides = guides.includes(:member).where(members: {gender: 0}) 
     end
     guides
