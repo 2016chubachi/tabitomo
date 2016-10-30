024c9d119b4262773242682b6e176ddfb5310c3a
aprisun <aprisun@gmail.com>
2016-10-30 23:24:04 +0900

    member05

--- db/migrate/20161012073814_create_members.rb
+++ db/migrate/20161012073814_create_members.rb
@@ -7,6 +7,7 @@ class CreateMembers < ActiveRecord::Migration
       t.integer :country_id
       t.string :telphone
       t.integer :gender
+      t.string :hantei
 
       t.timestamps null: false
     end
