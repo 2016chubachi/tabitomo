45bf98414428e70f222df64d3968843ddded59f7
takeshitou <takeshitou@gmail.com>
2016-11-05 05:25:06 +0000

    guide detail module v4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,48 @@ function language_click(){
   var lan_text = $('#language_change span:first-child').text();
   $("#language_change + ul li :contains('"+ lan_text.trim() +"')").parent().addClass('disabled');
 }
+
+function calendarDrawing(date,headTime){
+  'use strict';
+
+  headTime.attr("datetime",date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate());
+  headTime.text(date.getFullYear() + "-" + (date.getMonth()+1));
+  
+  var firstDate = new Date(date.getFullYear(), date.getMonth(), 1);
+  var lastDate = new Date(date.getFullYear(), date.getMonth()+1, 0);
+  var firstDayOfWeek = firstDate.getDay();
+  var arrDate = Array(42);
+  arrDate.fill(null);
+  for(var i = 0;i < lastDate.getDate();i++){
+    arrDate[i+firstDayOfWeek] = i+1;
+  }
+  var calendarHTML = "";
+  var trHTML = "";
+  
+  $.each(arrDate,function(index,value){
+    var temp = value;
+    firstDate = new Date(date.getFullYear(), date.getMonth(), 1);
+    
+    if(value === null){
+      if(index!=0 && index%7==0){
+        return false;
+      }
+      else{
+        firstDate.setDate(index + 1 - firstDayOfWeek);
+        temp = firstDate.getDate();
+      }
+      trHTML+='<td data-outside="1">'+ temp +'</td>';
+    }
+    else{
+      trHTML+='<td>'+ value +'</td>';
+    }
+    
+    if((index+1)%7==0){
+      // 周変わり
+      calendarHTML+='<tr>' + trHTML + '</tr>';
+      trHTML = "";
+    }
+  });
+  
+  $(".schdule-Calendar tbody").html(calendarHTML);
+}
--- app/controllers/guide_detail_controller.rb
+++ app/controllers/guide_detail_controller.rb
@@ -2,5 +2,11 @@ class GuideDetailController < ApplicationController
   def show
     @guide = Guide.find(params[:id])
     @reviews = UserReview.includes(:booking).where(bookings: {guide_id: params[:id]})
+    @revCount = @reviews.count
+    @fiveCount = @reviews.where(star_master_id: 5).count
+    @fourCount = @reviews.where(star_master_id: 4).count
+    @threeCount = @reviews.where(star_master_id: 3).count
+    @twoCount = @reviews.where(star_master_id: 2).count
+    @oneCount = @reviews.where(star_master_id: 1).count
   end
 end
