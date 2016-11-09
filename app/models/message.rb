594a9acd65655eaba4db576a158bb9f2eaf5ad08
aprisun <aprisun@gmail.com>
2016-11-09 20:35:06 +0900

    message02

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,19 @@ class Message < ActiveRecord::Base
     belongs_to :sender ,:class_name => "Member" , foreign_key: "sender_id"
     #受信者リレーション
     belongs_to :receiver ,:class_name => "Member" ,foreign_key: "receiver_id"
+    validates :title, :content, presence: true
+    validates :title, length: { maximum: 200 }
 
+    scope :readable_for, ->(member) {
+    member ? all : where(member_only: false) }
+
+    attr_accessor :member_only
+
+    # private
+    #
+    # class << self
+    #   def sidebar_articles(member, num = 5)
+    #     open.readable_for(member).order(released_at: :desc).limit(num)
+    #   end
+    # end
 end
