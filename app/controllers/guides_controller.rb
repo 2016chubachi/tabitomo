4eb3281dd4b303f63784b6e1686afd58167bff2b
shaori <shaori@foxmail.com>
2016-11-19 13:24:43 +0900

    fix guides erb

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -36,9 +36,11 @@ class GuidesController < ApplicationController
 
   private
     def guide_params
-          params.require(:guide).permit(
-                  :profile, :guide_service, :guide_transportation,
-                  :guide_interest, :experience, :license_flg,:birth_year,
-           )
+      params.require(:guide).permit(
+              :profile, :guide_service, :guide_transportation,
+              :guide_interest, :experience, :license_flg,:birth_year,
+              #licence_pictures_attributes: [:id, :photo],
+              member_attributes: [:gender, :first_name, :last_name,:telphone,:country_id]
+       )
       end
 end
