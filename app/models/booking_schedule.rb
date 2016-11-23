ca48a1e7a4f3882a3ceb45fd5e4be044c6fe0eb0
yamamotoAIIT <a1548sy@aiit.ac.jp>
2016-11-23 15:52:07 +0900

    [add]予約一覧（ガイド）の作成
    予約一覧（ガイド）の作成

--- app/models/booking_schedule.rb
+++ app/models/booking_schedule.rb
@@ -4,6 +4,7 @@ class BookingSchedule < ActiveRecord::Base
 
     validates :traveler_count, :traveler_date,:city_master_id,  presence: true
     validates :traveler_count, numericality: { only_integer: true, greater_than: 0}
+    # validates :traveler_count, numericality: { only_integer: true, greater_than: 0, message: :invalid_traveler_count }
     validates :traveler_date, uniqueness: {scope: "booking_id"}
 
 end
