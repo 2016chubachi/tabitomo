cc2c06289c77cbd4f4d240b9e0289cd83599159f
takeshitou <takeshitou@gmail.com>
2016-11-26 16:16:57 +0900

    message module

--- app/controllers/message_receives_controller.rb
+++ app/controllers/message_receives_controller.rb
@@ -1,2 +1,7 @@
 class MessageReceivesController < ApplicationController
+  # 受信メッセージ
+
+  def index
+    @receives = Message.find(:receiver_id => current_member,:select => "receiver_id, sender_id").uniq
+  end
 end
