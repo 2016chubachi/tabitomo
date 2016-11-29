d3e5748499a8b22e2207c46ed3474a18631adcab
takeshitou <takeshitou@gmail.com>
2016-11-29 08:40:22 +0000

    conflict fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # rescue_from Exception, with: :render_500
+  rescue_from Exception, with: :render_500
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
 
