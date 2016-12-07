dc6924b2e1cae55bc55b9e5ce42a494210249a4f
takeshitou <takeshitou@gmail.com>
2016-12-07 10:48:12 +0000

    guide profile modify

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,5 +1,5 @@
 class GuidesController < ApplicationController
-
+  before_action :authenticate_member!
   # def show
   #   @guide = Guide.find(params[:id])
   #   if params[:format].in?(["jpg", "png", "gif"])
@@ -17,12 +17,45 @@ class GuidesController < ApplicationController
 
   def update
     @guide = Guide.find(params[:id])
-    @guide.update_attributes(guide_params)
-    if @guide.save
-      flash[:success] = 'Succeed saving profile information.'
+    @guide.assign_attributes(guide_params)
+    # ライセンスのが[yes]の場合、画像のアップロードが必須
+    if @guide.license_flg == 1 && !@guide.licence_picture.present?
+      @guide.errors[:base] << "ライセンスに[yes]がチェックされた場合、ライセンス写真が必須です！"
+    end
+    # ガイド言語の重複チェック
+    langs = []
+    # for index in 0..1 do
+    @guide.guide_languages.each do |lan|
+      if lan._destroy == false && langs.include?(lan.language_code_id)
+        @guide.errors[:base] << "ガイド言語が重複しています！"
+      else
+        langs << (lan.language_code_id)
+      end 
+    end
+    # ガイドシティの重複チェック
+    cities = []
+    @guide.guide_cities.each do |c|
+      if c._destroy == false && cities.include?(c.city_master_id)
+        @guide.errors[:base] << "ガイド都市が重複しています！"
+      else
+        cities << (c.city_master_id)
+      end 
+    end
+    if @guide.errors.present?
+      # エラー情報を遷移先に渡す
+      session[:errors] = @guide.errors.full_messages
+      # メッセージ送信ページに遷移
       redirect_to edit_guide_path @guide
     else
-      render 'edit'
+      if @guide.save
+        flash[:success] = 'Succeed saving profile information.'
+        redirect_to edit_guide_path @guide
+      else
+        # エラー情報を遷移先に渡す
+        session[:errors] = @guide.errors.full_messages
+        # メッセージ送信ページに遷移
+        redirect_to edit_guide_path @guide
+      end
     end
   end
 
@@ -30,6 +63,8 @@ class GuidesController < ApplicationController
     @guide = Guide.find(params[:id])
     @guide.member.build_member_picture unless @guide.member.member_picture
     @guide.build_licence_picture unless @guide.licence_picture
+    # @guide.build_guide_languages unless @guide.guide_languages
+    # @guide.build_guide_cities unless @guide.guide_cities
   end
 
   def create
@@ -47,6 +82,8 @@ class GuidesController < ApplicationController
       attrs_Member = [:gender, :first_name, :last_name,:telphone,:country_id]
       attrs_Member << { member_picture_attributes: [:id, :uploaded_image,:_destroy]}
       attrs_Guide  << { member_attributes: attrs_Member}
+      attrs_Guide  << { guide_languages_attributes: [:id, :language_code_id,:language_skill_id,:_destroy]}
+      attrs_Guide  << { guide_cities_attributes: [:id, :city_master_id,:_destroy]}
       params.require(:guide).permit(attrs_Guide)
 
     end
--- app/models/guide.rb
+++ app/models/guide.rb
@@ -4,7 +4,9 @@ class Guide < ActiveRecord::Base
 
     has_many :bookings
     has_many :guide_cities
+    accepts_nested_attributes_for :guide_cities, allow_destroy: true
     has_many :guide_languages
+    accepts_nested_attributes_for :guide_languages, allow_destroy: true
 
     has_one :licence_picture, dependent: :destroy
     accepts_nested_attributes_for :licence_picture, allow_destroy: true
