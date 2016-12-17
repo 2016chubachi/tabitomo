86295b34c885618939e80668fa34339e950cc431
aprisun <aprisun@gmail.com>
2016-12-17 10:23:25 +0900

    added korean language

--- db/seeds/production/language_skills.rb
+++ db/seeds/production/language_skills.rb
@@ -1,2 +1,2 @@
 #言語熟練度
-LanguageSkill.create([{text_en: 'Primary',text_ja: '',text_ko: '',text_zh: ''},{text_en: 'basic',text_ja: '',text_ko: '',text_zh: ''},{text_en: 'fluent',text_ja: '',text_ko: '',text_zh: ''}])
+LanguageSkill.create([{text_en: 'Primary',text_ja: '',text_ko: '네이티브',text_zh: ''},{text_en: 'basic',text_ja: '',text_ko: '기초',text_zh: ''},{text_en: 'fluent',text_ja: '',text_ko: '유창',text_zh: ''}])
