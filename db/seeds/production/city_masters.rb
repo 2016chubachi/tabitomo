def767fe1428779c2da1757435ad9444515ccb76
yamamotoAIIT <a1548sy@aiit.ac.jp>
2016-12-17 17:06:24 +0900

    [update]table 変更
    city_mastersとcountriesに表示順(display_order)を追加

--- db/seeds/production/city_masters.rb
+++ db/seeds/production/city_masters.rb
@@ -3,35 +3,160 @@ CityMaster.create(
   text_ja: "東京",
   text_en: "Tokyo",
   text_ko: "도쿄",
-  text_zh: "东京"
+  text_zh: "东京",
+  display_order: 1
 )
 CityMaster.create(
   text_ja: "京都",
   text_en: "Kyoto",
   text_ko: "교토",
-  text_zh: "京都"
+  text_zh: "京都",
+  display_order: 2
 )
 CityMaster.create(
   text_ja: "奈良",
   text_en: "Nara",
   text_ko: "奈良_ko",
-  text_zh: "奈良"
+  text_zh: "奈良",
+  display_order: 3
 )
 CityMaster.create(
   text_ja: "広島",
   text_en: "Hiroshima",
   text_ko: "히로시마",
-  text_zh: "广岛"
+  text_zh: "广岛",
+  display_order: 4
 )
 CityMaster.create(
   text_ja: "沖縄",
   text_en: "Okinawa",
   text_ko: "오키나와",
-  text_zh: "冲绳"
+  text_zh: "冲绳",
+  display_order: 5
 )
 CityMaster.create(
   text_ja: "北海道",
   text_en: "Hokkaido",
   text_ko: "홋카이도",
-  text_zh: "北海道"
+  text_zh: "北海道",
+  display_order: 6
+)
+CityMaster.create(
+  text_ja: "大阪",
+  text_en: "Osaka",
+  text_ko: "大阪_ko",
+  text_zh: "大阪_xh",
+  display_order: 7
+)
+CityMaster.create(
+  text_ja: "富士山",
+  text_en: "Fujisan",
+  text_ko: "富士山_ko",
+  text_zh: "富士山_zh",
+  display_order: 8
+)
+CityMaster.create(
+  text_ja: "箱根",
+  text_en: "Hakone",
+  text_ko: "箱根_ko",
+  text_zh: "箱根_zh",
+  display_order: 9
+)
+CityMaster.create(
+  text_ja: "鎌倉",
+  text_en: "Kamakura",
+  text_ko: "鎌倉_ko",
+  text_zh: "鎌倉_zh",
+  display_order: 10
+)
+CityMaster.create(
+  text_ja: "金沢",
+  text_en: "Kanazawa(Ishikawa)",
+  text_ko: "金沢_ko",
+  text_zh: "金沢_zh",
+  display_order: 11
+)
+CityMaster.create(
+  text_ja: "高山",
+  text_en: "Takayama",
+  text_ko: "高山_ko",
+  text_zh: "高山_zh",
+  display_order: 12
+)
+CityMaster.create(
+  text_ja: "伊勢",
+  text_en: "Ise",
+  text_ko: "伊勢_ko",
+  text_zh: "伊勢_zh",
+  display_order: 13
+)
+CityMaster.create(
+  text_ja: "白川郷",
+  text_en: "Shirakawago",
+  text_ko: "白川郷_ko",
+  text_zh: "白川郷_zh",
+  display_order: 14
+)
+CityMaster.create(
+  text_ja: "日光",
+  text_en: "Nikko",
+  text_ko: "日光_ko",
+  text_zh: "日光_zh",
+  display_order: 15
+)
+CityMaster.create(
+  text_ja: "福岡",
+  text_en: "Fukuoka",
+  text_ko: "福岡_ko",
+  text_zh: "福岡_zh",
+  display_order: 16
+)
+CityMaster.create(
+  text_ja: "知床",
+  text_en: "Shiretoko",
+  text_ko: "知床_ko",
+  text_zh: "知床_zh",
+  display_order: 17
+)
+CityMaster.create(
+  text_ja: "高野山",
+  text_en: "Koyasan",
+  text_ko: "高野山_ko",
+  text_zh: "高野山_zh",
+  display_order: 18
+)
+CityMaster.create(
+  text_ja: "長野",
+  text_en: "Nagano",
+  text_ko: "長野_ko",
+  text_zh: "長野_zh",
+  display_order: 19
+)
+CityMaster.create(
+  text_ja: "別府",
+  text_en: "Beppu",
+  text_ko: "別府_ko",
+  text_zh: "別府_zh",
+  display_order: 20
+)
+CityMaster.create(
+  text_ja: "松山",
+  text_en: "Matsuyama",
+  text_ko: "松山_ko",
+  text_zh: "松山_zh",
+  display_order: 21
+)
+CityMaster.create(
+  text_ja: "宮島",
+  text_en: "Miyazima",
+  text_ko: "宮島_ko",
+  text_zh: "宮島_zh",
+  display_order: 22
+)
+CityMaster.create(
+  text_ja: "その他",
+  text_en: "Other",
+  text_ko: "その他_ko",
+  text_zh: "その他_zh",
+  display_order: 999999
 )
